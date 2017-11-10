00a016b385c7bc1a0dce7dd10682f260b7e706b5
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-10 12:14:19 +0000

    Init setupt with babel

---
+++ inDecision-app/public/scripts/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var template = React.createElement(
+  'p',
+  null,
+  'This is some JSX'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
