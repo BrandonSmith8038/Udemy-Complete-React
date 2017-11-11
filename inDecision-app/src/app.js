730e451c2a76c1bd74ecc42d51de0e9e7500525c
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-11 08:38:29 +0000

    Conditional Rendering In JSX

--- inDecision-app/src/app.js
+++ inDecision-app/src/app.js
@@ -1,5 +1,45 @@
-var template = <p>This is some JSX</p>
 var appRoot = document.getElementById('app');
 
+var app = {
+    title: 'My inDecision App',
+    subtitle: 'The Best App Eva',
+    options: ['Option One', 'Option Two']
+}
+
+var template = (
+
+<div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No Options'}</p>
+    <ol>
+        <li>Item One</li>
+        <li>Item Two</li>
+        <li>Item Three</li>
+    </ol>
+</div>
+)
+
+var user = {
+    name: 'Brandon',
+    age: 18,
+    location: 'Goodyear,AZ'
+}
+
+
+
+function getLocation(location){
+    if(location){
+        return <p>Location: {location}</p>
+    } 
+}
+
+var template2 = (
+    <div>
+        <h1>Name: {user.name ? user.name : 'Anonymous'}</h1>
+        {(user.age && user.age > 17) && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
+    </div>
+    )
 
 ReactDOM.render(template,appRoot)
