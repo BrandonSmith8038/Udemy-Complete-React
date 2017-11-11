8b24bdda8561c3df74a51dea41028ab2cade0341
Brandon Smith <reddirtwd@outlook.com>
2017-11-11 09:05:00 +0000

    Let and Const

--- inDecision-app/public/scripts/app.js
+++ inDecision-app/public/scripts/app.js
@@ -1,1 +1,85 @@
-"use strict";
+'use strict';
+
+var appRoot = document.getElementById('app');
+
+var app = {
+    title: 'My inDecision App',
+    subtitle: 'The Best App Eva',
+    options: ['Option One', 'Option Two']
+};
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        'p',
+        null,
+        app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No Options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item One'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Two'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item Three'
+        )
+    )
+);
+
+var user = {
+    name: 'Brandon',
+    age: 18,
+    location: 'Goodyear,AZ'
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            'p',
+            null,
+            'Location: ',
+            location
+        );
+    }
+}
+
+var template2 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Name: ',
+        user.name ? user.name : 'Anonymous'
+    ),
+    user.age && user.age > 17 && React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    getLocation(user.location)
+);
+
+ReactDOM.render(template, appRoot);
