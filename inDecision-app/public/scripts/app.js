aa29f6d635c3deb838be09d8d6290a14f4a1d82e
Brandon Smith <reddirtwd@outlook.com>
2017-11-11 08:38:29 +0000

    Conditional Rendering In JSX

--- inDecision-app/public/scripts/app.js
+++ inDecision-app/public/scripts/app.js
@@ -1,10 +1,1 @@
-'use strict';
-
-var template = React.createElement(
-  'p',
-  null,
-  'This is some JSX'
-);
-var appRoot = document.getElementById('app');
-
-ReactDOM.render(template, appRoot);
+"use strict";
