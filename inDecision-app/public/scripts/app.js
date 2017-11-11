7cfd526b56921bb8416ed5afc27275e3660e6f4c
Brandon Smith <reddirtwd@outlook.com>
2017-11-11 10:06:36 +0000

    Arrow Functions2

--- inDecision-app/public/scripts/app.js
+++ inDecision-app/public/scripts/app.js
@@ -1,25 +1,84 @@
 'use strict';
 
-// function squared(num) {
-//     return num * num
-// }
+// arguments object - no longer bound with arrow functions
 
-// console.log(squared(5))
-
-// const arrowSquared = (num) => {
-//     return num * num
-// }
-// console.log(arrowSquared(5))
-
-// const expressionSquare = (num) => num * num
-// console.log(expressionSquare(5))
-
-var getFirstNameVerbose = function getFirstNameVerbose(fullName) {
-    return fullName.split(' ')[0];
+var add = function add(a, b) {
+    console.log(arguments);
+    return a + b;
 };
-console.log('First Name Verbose: ', getFirstNameVerbose('Brandon Smith'));
+console.log(add(55, 1));
 
-var getFirstNameExpression = function getFirstNameExpression(fullName) {
-    return fullName.split(' ')[0];
+var arrowAdd = function arrowAdd(a, b) {
+    //Wont' Run
+    //console.log(arguments)
+    return a + b;
 };
-console.log('First Name Expression: ', getFirstNameExpression('Brandon Smith'));
+console.log(arrowAdd(55, 1));
+
+// this keyword - no longer boud
+var user = {
+    name: 'Brandon',
+    cities: ['Phoenix', 'Goodyear', 'Wittmann'],
+    printPlacesLived: function printPlacesLived() {
+        var _this = this;
+
+        console.log(this.name);
+        console.log(this.cities);
+
+        // //Won't Run = This Not Bound
+        // this.cities.forEach(function(city) {
+        //     console.log(this.name + ' has lived in' + city)
+        // })
+
+        this.cities.forEach(function (city) {
+            console.log(_this.name + ' has lived in ' + city);
+        });
+    }
+};
+
+user.printPlacesLived();
+
+//Methods Definition Syntax
+
+var user2 = {
+    name: 'Brandon',
+    cities: ['Phoenix', 'Goodyear', 'Wittmann'],
+    placesLived: function placesLived() {
+        var _this2 = this;
+
+        this.cities.forEach(function (city) {
+            console.log('For Each ', _this2.name + ' has lived in ' + city);
+        });
+    }
+};
+user2.placesLived();
+
+//.map() - Gives you a new array allowing you transform before using
+var user3 = {
+    name: 'Brandon',
+    cities: ['Phoenix', 'Goodyear', 'Wittmann'],
+    placesLived: function placesLived() {
+        var _this3 = this;
+
+        return this.cities.map(function (city) {
+            return _this3.name + ' has lived in ' + city;
+        });
+    }
+};
+console.log(user3.placesLived());
+
+// Challenge Area
+
+var multiplier = {
+    numbers: [1, 2, 3],
+    multiplyBy: 2,
+    multiply: function multiply() {
+        var _this4 = this;
+
+        return this.numbers.map(function (num) {
+            return num * _this4.multiplyBy;
+        });
+    }
+};
+
+console.log(multiplier.multiply());
