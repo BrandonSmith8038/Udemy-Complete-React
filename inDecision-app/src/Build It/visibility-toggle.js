6e60871e0beb726f27a251c2674e3e60d237f495
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-12 13:49:28 +0000

    Indecision State Part 1

--- inDecision-app/src/Build It/visibility-toggle.js
+++ inDecision-app/src/Build It/visibility-toggle.js
@@ -34,37 +34,3 @@ class VisibilityToggle extends React.Component {
 }
 
 ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
-
-
-/*const app = {
-    title: 'Visibility Toggle',
-    details: '',
-    showHide: 'Show Details'
-}
-
-const showHideDetails = () => {
-    app.details ? app.details = '' : app.details = 'These are the details'
-    app.details ? app.showHide = 'Hide Details' : app.showHide = 'Show Details'
-
-    appRender()
-}
-
-const appRender = () => {
-    const template = (
-
-        <div>
-        <h1>{app.title}</h1>
-        <button className="btn btn-primary" onClick={showHideDetails}>{app.showHide}</button>
-        <p className="mt-5">{app.details}</p>
-    </div>
-
-
-    )
-
-    const appRoot = document.getElementById('app');
-
-    ReactDOM.render(template, appRoot)
-
-}
-
-appRender()*/
