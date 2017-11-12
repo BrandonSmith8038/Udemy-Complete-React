b2396bcc76e5d2d300545a3f37434f1be59616c9
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-12 13:19:41 +0000

    Adding state to Build IT Visibility Toggle

--- inDecision-app/src/Build It/visibility-toggle.js
+++ inDecision-app/src/Build It/visibility-toggle.js
@@ -1,4 +1,42 @@
-const app = {
+class VisibilityToggle extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.handleClick = this.handleClick.bind(this)
+
+        this.state = {
+            visibility: false
+        }
+    }
+
+    handleClick() {
+
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            }
+        })
+    }
+
+    render() {
+
+
+        return (
+            <div>
+                <h1 className="mb-3">Visibility Toggle</h1>
+                <button className="btn mb-3" onClick={this.handleClick}>{this.state.visibility ? "Hide Details" : "Show Details"}</button>
+                <p>{this.state.visibility ? "These are some awesome details" : ""}</p>
+            </div>
+        )
+    }
+
+}
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
+
+
+/*const app = {
     title: 'Visibility Toggle',
     details: '',
     showHide: 'Show Details'
@@ -29,4 +67,4 @@ const appRender = () => {
 
 }
 
-appRender()
+appRender()*/
