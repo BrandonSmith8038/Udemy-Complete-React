2e6bb553168c4ceb00b636cdf68e1f4bd0049a29
Brandon Smith <reddirtwd@outlook.com>
2017-11-16 13:57:09 +0000

    Import/Export

---
+++ inDecision-app/src/utils.js
@@ -0,0 +1,12 @@
+console.log('Utils.js is Running!')
+
+
+export const square = function(x) {
+    return x * x
+}
+
+export const add = function(a, b) {
+    return a + b
+}
+
+//export { square, add }
