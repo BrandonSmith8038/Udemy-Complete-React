3bb77e3a967015438e34d9ab8c2babc21f4fc7d1
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-17 13:03:03 +0000

    Setting Up React Modal

--- inDecision-app/src/app.js
+++ inDecision-app/src/app.js
@@ -2,4 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import InDecisionApp from './components/InDecisionApp'
 
+
+
+
 ReactDOM.render(<InDecisionApp />, document.getElementById('app'))
