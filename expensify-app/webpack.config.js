e23284a4b5451c8bc88cc1bde03fba11b3892caa
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-18 13:16:09 +0000

    Basic routing

---
+++ expensify-app/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+
+module.exports = {
+    "entry": [
+        'webpack-dev-server/client?https://0.0.0.0:8080',
+        'webpack/hot/only-dev-server',
+        './src/app.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            use: [
+                "style-loader",
+                "css-loader",
+                "sass-loader"
+            ],
+            test: /\.s?css$/,
+        }]
+    },
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        disableHostCheck: true,
+        historyApiFallback: true,
+        hot: true,
+        host: process.env.IP,
+        //https: true,
+        port: process.env.PORT,
+        "public": "https://udemy-complete-react-cowboy8038.c9users.io" //no trailing slash
+    }
+}
