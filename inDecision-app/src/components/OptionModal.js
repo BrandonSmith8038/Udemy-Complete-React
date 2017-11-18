77c6cff4c9933ce1e452e2bb8d540cf7b10efdd5
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-18 09:37:03 +0000

    Styling React-Modal

--- inDecision-app/src/components/OptionModal.js
+++ inDecision-app/src/components/OptionModal.js
@@ -7,10 +7,12 @@ const OptionModal = (props) => (
         isOpen={!!props.selectedOption}
         onRequestClose={props.handleClose}        
         contentLabel="Selected Option"
+        closeTimeoutMS={400}
+        className="modal"
         >
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.handleClose}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.handleClose}>Okay</button>
     </Modal>
 )
 
