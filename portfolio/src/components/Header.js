20dd14feac0ec7fcd0c6b60c9f138923c7c7894a
Brandon Smith <reddirtwd@outlook.com>
2017-11-19 13:33:44 +0000

    Actions

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => (
-    <header>
+    <header className="mr-3">
         <h1>Portfolio</h1>
-        <NavLink exact={true} to="/" activeClassName="is-active">Home</NavLink>
-        <NavLink  to="/portfolio" activeClassName="is-active">Portfolio</NavLink>
+        <NavLink  exact={true} to="/" activeClassName="is-active">Home</NavLink>
+        <NavLink  to="/portfolio" activeClassName="is-active" exact={true}>Portfolio</NavLink>
         <NavLink  to="/contact" activeClassName="is-active">Contact</NavLink>
     </header>
 )
