f4bdcad2c03135c8a046e03f8e15427976dc69ae
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-19 10:53:38 +0000

    Build It - Portfolio

---
+++ portfolio/src/routers/AppRouter.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom'
+import NotFoundPage from '../components/NotFoundPage'
+import Header from '../components/Header'
+import PortfolioDashboard from '../components/PortfolioDashboard'
+import PortfolioItem from '../components/PortfolioItem'
+import Portfolio from '../components/Portfolio'
+import Contact from '../components/Contact'
+
+
+
+const AppRouter = () => (
+    <BrowserRouter>
+        <div>
+            <Header />
+            <Switch>
+                <Route path='/' component={PortfolioDashboard} exact={true}/>
+                <Route path="/portfolio/:id" component={PortfolioItem} />
+                <Route path='/portfolio' component={Portfolio} />
+                <Route path="/contact" component={Contact}/>
+                <Route component={NotFoundPage} />
+            </Switch>
+        </div>
+        
+    </BrowserRouter>
+)
+
+export default AppRouter
