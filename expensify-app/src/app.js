9c17d3d1bc375795c8744b49f180423adc6671eb
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-24 08:30:52 +0000

    Connecting store and component with react-redux

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import { addExpense } from './actions/expenses'
@@ -18,7 +19,11 @@ store.dispatch(addExpense({ description: 'Gas Bill', amount: 5, createdAt: -1000
 
 console.log(store.getState())
 
-store.dispatch(setTextFilter('gas'))
+store.dispatch(setTextFilter('water'))
+
+setTimeout(() => {
+    store.dispatch(setTextFilter('gas'))
+}, 3000)
 
 
 const state = store.getState()
@@ -27,6 +32,10 @@ console.log("visibleExpenses")
 console.log(visibileExpenses)
 
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
 
-
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+ReactDOM.render(jsx, document.getElementById('app'))
