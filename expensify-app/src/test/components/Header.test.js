995a40901eed9d819d8ba7025dafdbb426bcd5c5
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-25 13:45:22 +0000

    Enzyme

--- expensify-app/src/test/components/Header.test.js
+++ expensify-app/src/test/components/Header.test.js
@@ -1,11 +1,18 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow'
+import { shallow } from 'enzyme'
 import React from 'react'
 import Header from '../../components/Header'
+import toJSON from 'enzyme-to-json'
 
 test('Should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer()
-    renderer.render(<Header />)
+    const wrapper = shallow(<Header />)
 
-    expect(renderer.getRenderOutput()).toMatchSnapshot()
+    expect(toJSON(wrapper)).toMatchSnapshot()
+
+    //expect(wrapper.find('h1').text()).toBe('Expensify')
+
+    // const renderer = new ReactShallowRenderer()
+    // renderer.render(<Header />)
+
+    // expect(renderer.getRenderOutput()).toMatchSnapshot()
 
 })
