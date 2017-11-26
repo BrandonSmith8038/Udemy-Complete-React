39fc6658223b0f208dd0c7a49097ae565aa7f789
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-26 10:36:42 +0000

    Mocking Libaries With Jes

---
+++ expensify-app/src/test/components/ExpenseForm.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import ExpenseForm from '../../components/ExpenseForm'
+import expenses from '../fixtures/expenses'
+
+test('Should render expense form correctly', () => {
+    const wrapper = shallow(<ExpenseForm />)
+
+    expect(wrapper).toMatchSnapshot()
+})
+
+test('Should render expense form with data', () => {
+    const wrapper = shallow(<ExpenseForm expense={expenses[0]}/>)
+
+    expect(wrapper).toMatchSnapshot()
+})
