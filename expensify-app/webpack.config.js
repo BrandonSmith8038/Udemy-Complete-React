390d313172dc0f4f218bdab44fa1a48a331a59f7
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-27 06:59:13 -0700

    Setup Production Webpack Build

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,24 +1,45 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = env => {
   console.log('env', env);
 
   const isProduction = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
 
   return {
     entry: [
-      'webpack-dev-server/client?https://0.0.0.0:8080',
-      'webpack/hot/only-dev-server',
+      // 'webpack-dev-server/client?https://0.0.0.0:8080',
+      // 'webpack/hot/only-dev-server',
       './src/app.js'
     ],
     output: { path: path.join(__dirname, 'public'), filename: 'bundle.js' },
     module: {
       rules: [
         { loader: 'babel-loader', test: /\.js$/, exclude: /node_modules/ },
-        { use: ['style-loader', 'css-loader', 'sass-loader'], test: /\.s?css$/ }
+        {
+          test: /\.s?css$/,
+          use: CSSExtract.extract({
+            use: [
+              {
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
+        }
       ]
     },
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    plugins: [CSSExtract],
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
