f986a33cd6ba098796e6dcc8a9f846a5eae5e37f
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-27 05:52:01 -0700

    Webpack Production

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -12,6 +12,8 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
+console.log('Testing');
+
 const jsx = (
   <Provider store={store}>
     <AppRouter />
--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,42 +1,35 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = {
-  entry: [
-    "webpack-dev-server/client?https://0.0.0.0:8080",
-    "webpack/hot/only-dev-server",
-    "./src/app.js"
-  ],
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        use: ["style-loader", "css-loader", "sass-loader"],
-        test: /\.s?css$/
-      }
-    ]
-  },
-  devtool: "cheap-module-eval-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true,
-    stats: "errors-only",
-    overlay: {
-      errors: true,
-      warnings: true
+module.exports = env => {
+  console.log('env', env);
+
+  const isProduction = env === 'production';
+
+  return {
+    entry: [
+      'webpack-dev-server/client?https://0.0.0.0:8080',
+      'webpack/hot/only-dev-server',
+      './src/app.js'
+    ],
+    output: { path: path.join(__dirname, 'public'), filename: 'bundle.js' },
+    module: {
+      rules: [
+        { loader: 'babel-loader', test: /\.js$/, exclude: /node_modules/ },
+        { use: ['style-loader', 'css-loader', 'sass-loader'], test: /\.s?css$/ }
+      ]
+    },
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      stats: 'errors-only',
+      overlay: { errors: true, warnings: true }
     }
-    /* disableHostCheck: true,
+  };
+  /* disableHostCheck: true,
         hot: true,
         host: process.env.IP,
         //https: true,
         port: process.env.PORT,
         "public": "udemy-complete-react-cowboy8038.c9users.io" //no trailing slash */
-  }
 };
