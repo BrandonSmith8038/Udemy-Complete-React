a48fb66fb27828a7c48a0dcde533a5a716acaa52
Brandon Smith <brandon@reddirtwebdesign.com>
2017-11-27 05:01:20 -0700

    Fixed Test

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,43 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import AppRouter from './routers/AppRouter'
-import configureStore from './store/configureStore'
-import { addExpense } from './actions/expenses'
-import { setTextFilter, sortByAmount } from './actions/filters'
-import getVisibleExpenses from './selectors/expenses'
-import 'normalize.css/normalize.css'
-import './styles/styles.scss'
-import 'react-dates/lib/css/_datepicker.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import AppRouter from './routers/AppRouter';
+import configureStore from './store/configureStore';
+import { addExpense } from './actions/expenses';
+import { setTextFilter, sortByAmount } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
-
-
-const store = configureStore()
-
-
-store.dispatch(addExpense({ description: 'Water Bill', amount: 500, createdAt: 21000 }))
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 60000, createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'Internet Bill', amount: 25, createdAt: 3000 }))
-
-/*console.log(store.getState())
-
-store.dispatch(setTextFilter('water'))
-store.dispatch(sortByAmount())
-
-setTimeout(() => {
-    store.dispatch(setTextFilter('bill'))
-}, 3000)*/
-
-
-const state = store.getState()
-const visibileExpenses = getVisibleExpenses(state.expenses, state.filters)
-
-
+const store = configureStore();
 
 const jsx = (
-    <Provider store={store}>
-        <AppRouter />
-    </Provider>
-)
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
 
-ReactDOM.render(jsx, document.getElementById('app'))
+ReactDOM.render(jsx, document.getElementById('app'));
