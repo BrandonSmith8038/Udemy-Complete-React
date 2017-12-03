66fb588e2c9f4241d46539ed910ae500ac34b8eb
Brandon Smith <brandon@reddirtwebdesign.com>
2017-12-03 09:01:59 +0000

    Public Only Routes

--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -6,8 +6,8 @@ import { startLogout } from '../actions/auth'
 export const Header = ({ startLogout }) => (
   <header>
     <h1>Expensify</h1>
-    <NavLink exact={true} to="/dashboard" activeClassName="is-active">
-      Home
+    <NavLink to="/dashboard" activeClassName="is-active">
+      Dashboard
     </NavLink>
     <NavLink to="/create" activeClassName="is-active">
       Create Expense
